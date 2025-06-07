954b8b39e975d0359a553694b0c2c4cdf1cd243f
kulunu <kulunukesara88@gmail.com>
2025-06-07 21:46:29 +0530

    new commit05

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/patientss', patientRoutes);
+app.use('/api/patients', patientRoutes);
 app.use('/api/medicines', medicineRoutes);
 
 app.use(errorHandler);
